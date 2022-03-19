6ac95d53d6ff3092daa7dad0eeb4e29acff29d30
BeCool_Yolanda <szal.nicholas@gmail.com>
2022-03-19 19:18:56 +0100

    Create vm.sh

---
+++ vm.sh
@@ -0,0 +1,12 @@
+apt -Sy --needed \
+  qemu \
+  dhclient \
+  openbsd-netcat \
+  virt-viewer \
+  libvirt \
+  dnsmasq \
+  dmidecode \
+  ebtables \
+  virt-install \
+  virt-manager \
+  bridge-utils
