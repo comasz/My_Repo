157df4ee4449321a83ffd7a72f9e91e4830bd570
BeCool_Yolanda <szal.nicholas@gmail.com>
2022-03-20 22:45:32 +0100

    Update docker_install_debian.sh

--- docker_install_debian.sh
+++ docker_install_debian.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-apt-get remove docker docker-engine docker.io containerd runc && echo Done || echo Failed
-apt-get update && echo Done || echo Failed
+apt-get remove docker docker-engine docker.io containerd runc || echo --FAILED--
+apt-get update || echo --FAILED--
 apt-get install \
     ca-certificates \
     curl \
     gnupg \
-    lsb-release && echo Done || echo Failed
-curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    lsb-release || echo --FAILED--
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg || echo --FAILED--
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get update && echo Done || echo Failed
-apt-get install docker-ce docker-ce-cli containerd.io && echo Done || echo Failed
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null || echo --FAILED--
+apt-get update || echo --FAILED--
+apt-get install docker-ce docker-ce-cli containerd.io || echo --FAILED--
