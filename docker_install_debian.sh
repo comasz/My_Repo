1ed7f3ce88295c9096fc60c7133fe62fa6b28833
BeCool_Yolanda <szal.nicholas@gmail.com>
2022-03-20 22:43:32 +0100

    Update docker_install_debian.sh

--- docker_install_debian.sh
+++ docker_install_debian.sh
@@ -7,5 +7,8 @@ apt-get install \
     gnupg \
     lsb-release && echo Done || echo Failed
 curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update && echo Done || echo Failed
 apt-get install docker-ce docker-ce-cli containerd.io && echo Done || echo Failed
